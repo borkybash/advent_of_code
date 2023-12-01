76496dbb5c3680d1662783e341bac4d2a42ca5e1
borkybash <borkybash@protonmail.com>
2023-12-01 18:01:26 -0500

    Add day1 starting files

---
+++ 2023/day_01/trebuchet.py
@@ -0,0 +1,9 @@
+# Advent of Code 2023 - Day 1: Trebuchet?!
+
+# TODO: import data from input file
+
+# TODO: parse to remove letters
+
+# TODO: get first and last number and combine and add to list
+
+# TODO: sum all numbers
